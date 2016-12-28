108b1300f061fa0622b8aa8acb643b5e042e21dd
Ben Murphy <ben@constellation.software>
2016-12-28 11:58:14 -0500

    Inverse link Audio -> Enclosure

--- PeaPod/Enclosure.swift
+++ PeaPod/Enclosure.swift
@@ -14,6 +14,9 @@ class Enclosure: Object {
     dynamic var url: String = ""
     dynamic var type: String = ""
     let length: RealmOptional<Int64> = RealmOptional(nil)
+
+    /// Inverse relationship to locally saved audio
+    let localAudio = LinkingObjects(fromType: Audio.self, property: "enclosure")
 }
 
 
