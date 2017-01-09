0c62ee8f018f84370c81d5e7412a2a50489bc8b1
Ben Murphy <ben@constellation.software>
2017-01-09 16:46:35 -0500

    Running Quick tests are a lot faster / easier
    Need to review DI ideas to make sure they're being built well

---
+++ SweetPeaTests/SubscribeViewModelSpec.swift
@@ -0,0 +1,37 @@
+//
+//  SubscribeViewModelSpec.swift
+//  SweetPea
+//
+//  Created by Ben Murphy on 1/9/17.
+//  Copyright © 2017 Constellation Software. All rights reserved.
+//
+
+import Quick
+import Nimble
+import RxSwift
+import FeedKit
+import RxBlocking
+@testable import SweetPea
+
+class SubscribeViewModelSpec: QuickSpec {
+    override func spec() {
+        let testBundle = Bundle(for: type(of: self))
+        let url = testBundle.url(forResource: "utr", withExtension: "rss")
+
+        let viewModel = SubscribeViewModel()
+        describe("Fetching an RSS Feed") {
+            it("can fetch a valid rss feed from a url") {
+                viewModel.podcastTitle
+                    .subscribe(onNext: { n in
+                        expect(n).to(match("Under the Radar"))
+                    })
+                viewModel.podcastDescription
+                    .subscribe(onNext: { n in
+                        expect(n.characters.count).to(beGreaterThan(10))
+                    })
+
+                viewModel.urlText.value = (url?.absoluteString)!
+            }
+        }
+    }
+}
